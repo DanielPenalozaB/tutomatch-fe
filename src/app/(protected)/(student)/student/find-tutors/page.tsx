4c4f00a9df36bb93ded89fd2937706942df17191
danieltoo <danielduranmadrid71@gmail.com>
2025-05-18 23:20:25 -0500

    Creación interfaz HU9

---
+++ src/app/(protected)/(student)/student/find-tutors/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import ContentSection from '@/components/layout/content-section';
+
+
+export default function FindTutorsPage() {
+  return (
+    <ContentSection
+      title='Solicitud tutoria'
+      desc='Gestiona aqui tus solicitudes de tutoria.'
+    >
+      <div>Pagina de solicitud de tutorias</div>
+    </ContentSection>
+  );
+}
